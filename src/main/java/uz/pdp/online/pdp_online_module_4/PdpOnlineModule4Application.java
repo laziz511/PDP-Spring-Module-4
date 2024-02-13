8c7e3a843006150e97c92c8d18daef921c0873c2
Laziz <lazizo2004@gmail.com>
2024-02-13 16:22:37 +0500

    write queries in queries.md

---
+++ src/main/java/uz/pdp/online/pdp_online_module_4/PdpOnlineModule4Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.pdp_online_module_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpOnlineModule4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdpOnlineModule4Application.class, args);
+	}
+
+}
---
+++ src/test/java/uz/pdp/online/pdp_online_module_4/PdpOnlineModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.pdp_online_module_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdpOnlineModule4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
